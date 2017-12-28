bdbb8119624d7540dba98505de507bdcb061de4d
ankit.sinha <ankit.sin@media.net>
2017-12-28 12:42:28 +0530

    Use CORS  middleware and auth

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,3 +1,33 @@
-module.exports = function(req, res, next) {
-    next();
+const jwt = require('../jwtToken');
+
+module.exports = {
+    CORS: function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Authorization, Content-Type, Accept'
+        );
+        next();
+    },
+
+    auth: (req, res, next) => {
+        const authHeader = req.get('Authorization');
+        const authHeaderArray = authHeader.split(' ');
+        if (authHeaderArray.length === 2 && authHeaderArray[0] === 'Bearer') {
+            const decodedToken = jwt.verifyToken(authHeaderArray[1]);
+
+            if (!decodedToken) {
+                res.json({
+                    error: 'Invalid Token'
+                });
+            } else {
+                req.user = decodedToken;
+                next();
+            }
+        } else {
+            res.json({
+                error: 'Invalid Token'
+            });
+        }
+    }
 };
